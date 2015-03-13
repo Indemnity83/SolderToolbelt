06cfdc47a6e31a8934a04cc12c5743f68d5dc3ec
Indemnity83 <kklaus@indemnity83.com>
2015-03-13 14:17:02 -0700

    Refactor mod and modpack commands for retrieving information from api; added get command for mods to download package.

--- src/ModCommand.php
+++ src/ModCommand.php
@@ -18,66 +18,121 @@ class ModCommand extends Command {
 	protected function configure()
 	{
 		$this->setName('mod')
-         ->setDescription('Work with mods on the TechnicSolder site')
-         ->addArgument(
-           'modname',
-           InputArgument::REQUIRED,
-           'The slug of the mod you wish to request'
-         )
-         ->addArgument(
-           'modversion',
-           InputArgument::OPTIONAL,
-           'Specific modversion you wish to view'
-         );
+			->setDescription('Work with mods on the TechnicSolder site')
+			->addArgument(
+					'action',
+					InputArgument::REQUIRED,
+					'info, get'
+				)
+			->addArgument(
+				 'slug',
+				 InputArgument::REQUIRED,
+				 'The slug of the mod you wish to request'
+			)
+			->addArgument(
+				 'version',
+				 InputArgument::OPTIONAL,
+				 'Specific modversion you wish to view'
+			);
 	}
 
 	/**
 	 * Execute the command.
 	 *
-	 * @param  \Symfony\Component\Console\Input\InputInterface  $input
-	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	 * @param	\Symfony\Component\Console\Input\InputInterface	$input
+	 * @param	\Symfony\Component\Console\Output\OutputInterface	$output
 	 * @return void
 	 */
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
-    $client = new Client();
-    $modname = $input->getArgument('modname');
-    $modversion = $input->getArgument('modversion');
-    $config = solder_config();
+		$commandAction = $input->getArgument('action');
+		$modSlug = $input->getArgument('slug');
+		$modVersion = $input->getArgument('version');
 
-    $response = $client->get($config->api);
-    $server = $response->json();
+		switch( $commandAction ) {
+			case 'info':
+				$this->infoMod($output, $modSlug, $modVersion);
+				break;
+			case 'get':
+				$this->getMod($output, $modSlug, $modVersion);
+				break;
+			default:
+				throw new \InvalidArgumentException('Invalid arguments');
+		}
+	}
 
-    $response = $client->get($config->api.'/mod/'.$modname.'/'.$modversion);
-    $json = $response->json();
+	private function infoMod($output, $slug, $version)
+	{
+		$this->displayServerInfo($output);
 
-    if(isset($json['error'])) {
-      throw new \Exception($json['error']);
-    }
+		$apiClient = new Client();
+		$appConfig = solder_config();
 
-    $rows = array();
-    foreach( $json as $key => $value ) {
-      if( $key == 'versions' ) {
-        $rows[] = array("<info>$key</info>", implode($value,"\n"));
-      } else {
-        $rows[] = array("<info>$key</info>", mb_strimwidth($value, 0, 80, "..."));
-      }
-    }
+		$apiResponse = $apiClient->get($appConfig->api . '/mod/' . $slug . '/' . $version)->json();
+		if(isset($apiResponse['error'])) {
+			throw new \Exception($apiResponse['error']);
+		}
 
-    $output->writeln('<comment>Server:</comment>');
-    $output->writeln(" <info>{$server['api']}</info> version {$server['version']}");
-    $output->writeln(" $api");
+		$rows = array();
+		foreach( $apiResponse as $key => $value ) {
+			if( $key == 'versions' ) {
+				$rows[] = array("<info>$key</info>", implode($value,"\n"));
+			} else {
+				$rows[] = array("<info>$key</info>", mb_strimwidth($value, 0, 60, "..."));
+			}
+		}
 
-    $output->writeln('');
-    $output->writeln("<comment>Mod:</comment>");
-    $table = new Table($output);
-    $table
-        ->setRows($rows)
-        ->setStyle('compact')
-        ->render();
+		$output->writeln('');
+		$output->writeln("<comment>Mod:</comment>");
+		$table = new Table($output);
+		$table
+				->setRows($rows)
+				->setStyle('compact')
+				->render();
+	}
 
+	private function getMod($output, $slug, $version)
+	{
+		if($slug == '' || $version == '') {
+			throw new \InvalidArgumentException('Invalid arguments');
+		}
 
+		$apiClient = new Client();
+		$appConfig = solder_config();
 
+		$apiResponse = $apiClient->get($appConfig->api . '/mod/' . $slug . '/' . $version)->json();
+		if(isset($apiResponse['error'])) {
+			throw new \Exception($apiResponse['error']);
+		}
+
+		$url = $apiResponse['url'];
+		$filename = basename($url);
+		$md5 = $apiResponse['md5'];
+
+		downloadFile($url, $filename, $output, $md5);
+
+		if( md5_file($filename) != $md5 ) {
+			throw new \Exception('Hash dosen\'t match');
+		}
+	}
+
+	private function displayServerInfo($output)
+	{
+		$apiClient = new Client();
+		$appConfig = solder_config();
+
+		$technicSolder = $apiClient->get($appConfig->api)->json();
+		if(isset($technicSolder['error'])) {
+			throw new \Exception($technicSolder['error']);
+		}
+
+		$output->writeln('<comment>Server:</comment>');
+		$output->writeln(" <info>{$technicSolder['api']}</info> version {$technicSolder['version']}");
+		$output->writeln(" {$appConfig->api}");
+
+		$apiClient = null;
+		$appConfig = null;
+		$technicSolder = null;
 	}
 
 }
